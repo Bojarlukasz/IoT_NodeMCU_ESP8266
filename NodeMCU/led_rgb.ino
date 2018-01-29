9ffe268fec39f115c06b951069ba0ee85155ca30
Bodzaro <Bojarlukasz@gmail.com>
2018-01-29 23:18:53 +0100

    Add files via upload

---
+++ NodeMCU/led_rgb.ino
@@ -0,0 +1,34 @@
+void rgb(float temp){
+  
+  if(led_rgb_activated == 1){
+    
+    if(temp >= 22.0){
+      analogWrite(red, 0);
+      analogWrite(green, 1000);
+      analogWrite(blue, 1000);
+    }else if(temp < 22.0 && temp >= 20.9){
+      analogWrite(red, 0);
+      analogWrite(green, 0);
+      analogWrite(blue, 1000);
+    }else if(temp < 20.9 && temp >= 20.0){
+      analogWrite(red, 1000);
+      analogWrite(green, 0);
+      analogWrite(blue, 1000);
+    }else if(temp < 20.0 && temp >= 19.0){
+      analogWrite(red, 1000);
+      analogWrite(green, 0);
+      analogWrite(blue, 0);
+    }else if(temp < 19.0 && temp >= 17.0){
+      analogWrite(red, 1000);
+      analogWrite(green, 1000);
+      analogWrite(blue, 0);
+    }else if(temp < 17.0){
+      analogWrite(red, 0);
+      analogWrite(green, 1000);
+      analogWrite(blue, 0);
+    }
+    
+  }
+  
+}
+
---
+++ NodeMCU/send_get.ino
@@ -0,0 +1,33 @@
+bool send_get(WiFiClient cl, String who, String msq_GET){
+  
+  if(msq_GET != "" and who != ""){
+    String ip = "";
+
+   if(cl.connect(nodejs_ip, nodejs_port)) {
+      
+      Serial.println("connected to Nodejs");
+      cl.println(msq_GET);
+      cl.println("Host: " + (String)nodejs_ip);
+      cl.println("Connection: close");
+      cl.println();
+
+      String req = cl.readStringUntil('\r');
+      cl.flush();
+
+      Serial.println(req);
+
+      if(req == "HTTP/1.1 200 OK"){
+        return true;
+      }else{
+        return false;
+      }
+      
+      
+    } else {
+      Serial.println("connection failed - Nodejs");
+      return false;
+    }
+
+  }
+
+}
